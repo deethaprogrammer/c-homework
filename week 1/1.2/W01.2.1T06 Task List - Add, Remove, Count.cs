9c2c30b6cf7c9638c31789e845b8c276ffc5552f
Deethaprogrammer <vdheidendamian@gmail.com>
2026-02-11 13:40:27 +0100

    01.2.t06 done

--- week 1/1.2/W01.2.1T06 Task List - Add, Remove, Count.cs
+++ week 1/1.2/W01.2.1T06 Task List - Add, Remove, Count.cs
@@ -2,6 +2,21 @@ static class Program
 {
     static void Main()
     {
-        
+        List<string> list_string = new();
+        PrintItem(list_string);
+        list_string.Add("Mow lawn");
+        list_string.Add("Pay taxes");
+        PrintItem(list_string);
+        bool removed = list_string.Remove("Mow lawn");
+        list_string.Add("Shopping");
+        PrintItem(list_string);
+    }
+    static void PrintItem(List<string> list_string)
+    {
+        Console.WriteLine($"Amount of tasks: {list_string.Count}");
+        foreach (var item in list_string)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
